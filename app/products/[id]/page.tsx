d654c877dee285c06de84129933276e9686a9e8b
mrirfanto <mrirfanto.work@gmail.com>
2025-06-26 15:26:23 +0700

    refactor: remove getStaticProduct from product detail page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,12 +4,7 @@ import ProductDetail from '@features/products/components/product-detail';
 import { mockProducts } from '@/shared/config/mockData';
 import { Product } from '@/shared/types/product';
 
-export async function generateStaticParams() {
-  const productIds = ['1', '2', '3', '4', '5'];
-  return productIds.map((id) => ({ id }));
-}
-
-async function getStaticProduct(id: string): Promise<Product | null> {
+async function getProduct(id: string): Promise<Product | null> {
   const product = mockProducts.find((product) => product.id === id);
 
   if (product) return product;
@@ -19,7 +14,7 @@ async function getStaticProduct(id: string): Promise<Product | null> {
 
 export default async function ProductDetailPage({ params }: { params: { id: string } }) {
   const { id } = await params;
-  const product = await getStaticProduct(id);
+  const product = await getProduct(id);
 
   if (!product) {
     return notFound();
